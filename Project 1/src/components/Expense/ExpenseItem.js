324d503e43776a1c30b5cc8b78f4beaeeefb24ed
Alfred Lin <>
2022-07-06 22:48:00 +0800

    useState test

--- Project 1/src/components/Expense/ExpenseItem.js
+++ Project 1/src/components/Expense/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpesneDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -10,13 +12,25 @@ const ExpenseItem = (props) => {
   //   const month = props.date.toLocaleString("en-US", { month: "long" });
   //   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   //   const year = props.date.getFullYear();
+  const [item, setItem] = useState(props.title);
+
+  const clickHandler = () => {
+    setItem("I did it!!");
+    console.log(item);
+  };
+
+  console.log("excuted at component level");
+  console.log(item);
 
   return (
     <Card className="expense-item">
       <ExpesneDate date={props.date}></ExpesneDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{item}</h2>
         <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <div>
+        <button onClick={clickHandler}>click</button>
       </div>
     </Card>
   );
